f466495fce8f4fc0d46632cb72f72169df9bef1d
Johnny-Oleg <neojin.entity@gmail.com>
2020-10-28 13:15:39 +0300

    some changes were made

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,38 @@
 import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import ReactDOM from 'react-dom';
+import renderer from 'react-test-renderer'; 
+import Enzyme, { shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import App, { Search, Button, Table } from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+Enzyme.configure({ adapter: new Adapter() });
+
+describe('Table', () => {
+  const props = {
+    list: [
+      { title: '1', author: '1', num_comments: 1, points: 2, objectID: 'y' },
+      { title: '2', author: '2', num_comments: 1, points: 2, objectID: 'z' }, 
+    ],
+    sortKey: 'TITLE',
+    isSortReverse: false,
+  };
+
+  it('отрисовывает без ошибки', () => {
+    const div = document.createElement('div'); 
+    ReactDOM.render(<Table { ...props } />, div);
+  });
+
+  it('shows two items in list', () => {
+    const element = shallow(
+      <Table { ...props } /> 
+    );
+    expect(element.find('.table-row').length).toBe(2); 
+  });
+
+  test('есть корректный снимок', () => { 
+    const component = renderer.create(
+      <Table { ...props } /> 
+    );
+    const tree = component.toJSON();
+    expect(tree).toMatchSnapshot(); });
 });
